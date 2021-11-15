ecb3998fa9231410ec011dd2eb7543653aa20119
sergo <s.volkov.office@gmail.com>
2021-11-15 22:42:40 +0300

    made basic entrypoints for retrieving the list, single entry of question, and creating, updating and deleting entries

--- test_task/main/views.py
+++ test_task/main/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework import viewsets, mixins, status
 
 from main.serializers import PollSerializer, QuestionSerializer
-from main.models import Poll
+from main.models import Poll, Question
 
 
 class TestView(APIView):
@@ -46,6 +46,47 @@ class PollsDetail(APIView):
     def post(self, request, pk, format=None):
         queryset = Poll.objects.get(pk=pk)
         serializer = PollSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        queryset = Poll.objects.get(pk=pk)
+        if queryset:
+            queryset.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuestionsList(APIView):
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request, format=None):
+        queryset = Question.objects.all()
+        serializer = QuestionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuestionsDetail(APIView):
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request, pk, format=None):
+        queryset = Question.objects.get(pk=pk)
+        serializer = QuestionSerializer(queryset)
+        return Response(serializer.data)
+
+    def post(self, request, pk, format=None):
+        queryset = Question.objects.get(pk=pk)
+        serializer = QuestionSerializer(queryset, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
